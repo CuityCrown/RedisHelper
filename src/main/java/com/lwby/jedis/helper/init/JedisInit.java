344b41f50851e4926cdc228e8f036bc6ac8afdae
yibo.liu <1501986252@qq.com>
2021-01-25 21:34:22 +0800

    redis工具类处理

---
+++ src/main/java/com/lwby/jedis/helper/init/JedisInit.java
@@ -0,0 +1,23 @@
+package com.lwby.jedis.helper.init;
+
+import com.lwby.jedis.helper.config.JedisConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * spring启动配置类 jedis相关
+ *
+ * @author 刘一博
+ * @version JedisConfiguration.java, v 0.1 2020年5月15日 10:51 刘一博 Exp $
+ */
+@Configuration
+public class JedisInit {
+
+    @Bean
+    @ConfigurationProperties(value = "jedis.redis")
+    public JedisConfig createJedisConfig() {
+        return new JedisConfig();
+    }
+
+}
